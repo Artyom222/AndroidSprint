4145baacd41ec7dfe8bc4dbf41c4c114585a64ba
APecherkin <pech.artyom2@yandex.ru>
2025-11-09 19:58:40 +0500

    RA-11

--- app/src/main/java/ru/example/androidsprint/Constants.kt
+++ app/src/main/java/ru/example/androidsprint/Constants.kt
@@ -3,3 +3,6 @@ package ru.example.androidsprint
 const val ARG_CATEGORY_ID = "category_id"
 const val ARG_CATEGORY_NAME = "category_name"
 const val ARG_CATEGORY_IMAGE_URL = "category_image_url"
+const val ARG_RECIPE_ID = "recipe_id"
+const val ARG_RECIPE_NAME = "recipe_name"
+const val ARG_RECIPE_IMAGE_URL = "recipe_image_url"
