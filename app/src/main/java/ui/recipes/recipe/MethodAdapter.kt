910e57e16bf5ad84378990041e29998f851ea39c
APecherkin <pech.artyom2@yandex.ru>
2026-01-19 21:09:48 +0500

    RA-26

--- app/src/main/java/ui/recipes/recipe/MethodAdapter.kt
+++ app/src/main/java/ui/recipes/recipe/MethodAdapter.kt
@@ -5,7 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import ru.example.androidsprint.databinding.ItemMethodBinding
 
-class MethodAdapter(private var dataSet: List<String>) :
+class MethodAdapter(var dataSet: List<String>) :
     RecyclerView.Adapter<MethodAdapter.ViewHolder>() {
 
     class ViewHolder(val binding: ItemMethodBinding) : RecyclerView.ViewHolder(binding.root)
