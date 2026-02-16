44d5dbff43668e7789be0120ce7800989d8c9017
APecherkin <pech.artyom2@yandex.ru>
2026-02-16 21:10:39 +0500

    RA-33

--- app/src/main/java/ui/MainActivity.kt
+++ app/src/main/java/ui/MainActivity.kt
@@ -9,10 +9,15 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
 import kotlinx.serialization.json.Json
 import model.Category
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
 import ru.example.androidsprint.R
 import ru.example.androidsprint.databinding.ActivityMainBinding
-import java.net.URL
-import javax.net.ssl.HttpsURLConnection
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -41,20 +46,33 @@ class MainActivity : AppCompatActivity() {
             findNavController(R.id.mainContainer).navigate(R.id.favoritesFragment)
         }
 
-        val thread = Thread {
-            val url = URL("https://recipes.androidsprint.ru/api/category")
-            val connection = url.openConnection() as HttpsURLConnection
-            connection.connect()
-
-            Log.i("!!!", "Выполняется запрос на потоке: ${Thread.currentThread().name}")
-            Log.i("!!!", "responseCode: ${connection.responseCode}")
-            Log.i("!!!", "responseMessage: ${connection.responseMessage}")
-            val categories = Json.decodeFromString<List<Category>>("${connection.inputStream.bufferedReader().readText()}")
-            categories.forEach { category ->
-                Log.i("!!!", "Категория: ${category.title}, id: ${category.id}")
-            }
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
         }
-        thread.start()
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
 
+        val request = Request.Builder()
+            .url("https://recipes.androidsprint.ru/api/category")
+            .get()
+            .build()
+
+        client.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("!!!", "Ошибка запроса: ${e.message}")
+            }
+            override fun onResponse(call: Call, response: Response) {
+                Log.i("!!!", "Выполняется запрос на потоке: ${Thread.currentThread().name}")
+                Log.i("!!!", "responseCode: ${response.code}")
+                Log.i("!!!", "responseMessage: ${response.message}")
+
+                val body = response.body?.string() ?: return
+                val categories = Json.decodeFromString<List<Category>>(body)
+                categories.forEach { category ->
+                    Log.i("!!!", "Категория: ${category.title}, id: ${category.id}")
+                }
+            }
+        })
     }
 }
